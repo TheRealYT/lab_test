41ebf21f1429dc7aca6421f31f81ddb23e1a0659
TheRealYT <yt040448@gmail.com>
2023-12-26 08:49:50 +0000

    init

---
+++ file.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string* read_names(string f_name, int len) {
+    string *names = new string[len];
+
+    fstream file;
+    file.open(f_name, ios::in);
+    if (file.is_open()) {
+        for (int i = 0; i < len && !file.eof(); ++i) {
+            string name;
+            file >> name;
+            names[i] = name;
+        }
+    }
+
+    return names;
+}
+
+int main() {
+    string *data = read_names("name.txt", 5);
+    for (int i = 0; i < 5; ++i) {
+        cout << data[i] << endl;
+    }
+}
